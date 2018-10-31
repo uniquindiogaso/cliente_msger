154867d58c5ec1070b91aaa4c4138d19a608109a
Capriatto <michu-1@hotmail.com>
2018-10-31 10:38:38 -0500

    archivo de conexión con server -C

--- src/project4/Connection.java
+++ src/project4/Connection.java
@@ -9,114 +9,36 @@ package project4;
  *
  * @author Juan
  */
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.SocketTimeoutException;
+import java.net.URLEncoder;
 
-/**
- *
- *
- */
-public class Connection
-{
+public class Connection {
 
-  public static void main(String[] args)
-  {
-    new Connection();
-  }
-  
-  public Connection()
-  {
-    String testServerName = "localhost";
-    int port = 6969;
-    try
-    {
-      // Abre un socketGET /\n\n"
-      Socket socket = openSocket(testServerName, port);
-      
-      // write-to, and read-from the socket.
-      // Escribiendo un comando simple al servidor web
-      String result = writeToAndReadFromSocket(socket, "hola :D");
-      
-      // Pintando el resultado de la respuesta del server
-      System.out.println(result);
+    public static void main(String[] argv) throws Exception {
+        String data = URLEncoder.encode("key1", "UTF-8") + "=" + URLEncoder.encode("value1", "UTF-8");
 
-      // Cerrando el socket
-      socket.close();
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
-  }
-  
-  private String writeToAndReadFromSocket(Socket socket, String writeTo) throws Exception
-  {
-    try 
-    {
-      // write text to the socket
-      BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-      bufferedWriter.write(writeTo);
-      bufferedWriter.flush();
-      
-      // read text from the socket
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      StringBuilder sb = new StringBuilder();
-      String str;
-      while ((str = bufferedReader.readLine()) != null)
-      {
-        sb.append(str + "\n");
-      }
-      
-      // close the reader, and return the results as a String
-      bufferedReader.close();
-      return sb.toString();
-    } 
-    catch (IOException e) 
-    {
-      e.printStackTrace();
-      throw e;
-    }
-  }
-  
-  /**
-   * Open a socket connection to the given server on the given port.
-   * This method currently sets the socket timeout value to 10 seconds.
-   * (A second version of this method could allow the user to specify this timeout.)
-   */
-  private Socket openSocket(String server, int port) throws Exception
-  {
-    Socket socket;
-    
-    // creando un socket con tiempo máximo de espera
-    try
-    {
-      InetAddress inteAddress = InetAddress.getByName(server);
-      SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
-  
-      // Creando un socket
-      socket = new Socket();
-  
-      // Tiempo máximo permitido 10 segundos
-      int timeoutInMs = 10*1000;  
-      socket.connect(socketAddress, timeoutInMs);
-      
-      return socket;
-    } 
-    catch (SocketTimeoutException ste) 
-    {
-      System.err.println("Tiempo agotado esperando respuesta del socket.");
-      ste.printStackTrace();
-      throw ste;
-    }
-  }
+        Socket socket = new Socket("127.0.0.1", 8000);
 
+        String path = "/status";
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+        wr.write("POST " + path + " HTTP/1.0\r\n");
+        wr.write("Content-Length: " + data.length() + "\r\n");
+        wr.write("Content-Type: application/x-www-form-urlencoded\r\n");
+        wr.write("\r\n");
+
+        wr.write(data);
+        wr.flush();
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            System.out.println(line);
+        }
+        wr.close();
+        rd.close();
+    }
 }
